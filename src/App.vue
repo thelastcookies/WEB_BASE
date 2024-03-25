9ca9152d8e4f4845794417e0fc58106cfc8c5a47
thelastcookies <thelastcookies@icloud.com>
2024-03-25 23:59:25 +0800

    chore: 修改默认 logo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
     <div>
         <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo"/>
+            <img src="/logo.svg" class="logo" alt="Vite logo"/>
         </a>
         <a href="https://vuejs.org/" target="_blank">
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
@@ -15,16 +15,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646CFFAA);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42B883AA);
-}
+
 </style>
