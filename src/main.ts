e9e65dcac44ad568b7844af66f7ba9ee5f437333
thelastcookies <thelastcookies@icloud.com>
2024-03-26 00:05:18 +0800

    chore: 新增全局样式表与字体

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app');
+// 全局样式表
+import '@/assets/index.less';
+
+export const app = createApp(App);
+
+app.mount('#app');
