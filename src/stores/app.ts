c54a26cdfc7a51a6a99540d801881882eb2c5456
thelastcookies <thelastcookies@icloud.com>
2024-03-26 01:26:31 +0800

    feat: 新增 App 应用状态管理

---
+++ src/stores/app.ts
@@ -0,0 +1,42 @@
+import {App} from 'ant-design-vue';
+import type {MessageInstance} from 'ant-design-vue/es/message/interface';
+import type {ModalStaticFunctions} from 'ant-design-vue/es/modal/confirm';
+import type {NotificationInstance} from 'ant-design-vue/es/notification/interface';
+
+export const useAppStore = defineStore('app', () => {
+    const windowInnerWidth = ref(0);
+    const windowInnerHeight = ref(0);
+
+    const message = ref({} as MessageInstance);
+    const notification = ref({} as NotificationInstance);
+    const modal = ref({} as Omit<ModalStaticFunctions, 'warn'>);
+    (() => {
+        const staticFunction = App.useApp();
+        message.value = staticFunction.message;
+        notification.value = staticFunction.notification;
+        modal.value = staticFunction.modal;
+    })();
+
+    const signOut = () => {
+        return new Promise(() => {
+            const actionStore = useActionStore();
+            actionStore.$reset();
+            const userStore = useUserStore();
+            userStore.$reset();
+            const tokenStore = useTokenStore();
+            tokenStore.$reset();
+            if (import.meta.env.APP_LOGIN_ENABLE === true) {
+                router.push("/login");
+            } else {
+                parent.window.location.assign("");
+            }
+        });
+    };
+
+    return {
+        windowInnerWidth,
+        windowInnerHeight,
+        signOut,
+        message, notification, modal,
+    }
+});
