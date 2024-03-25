5395fbcc2107327aa820eedd47f1f0549dde0b85
thelastcookies <thelastcookies@icloud.com>
2024-03-26 02:08:36 +0800

    feat: 在应用级别引入深浅色 antdv 主题 与 CSS 变量，实现主题切换功能
    Release-As: 0.2.0

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {legacyLogicalPropertiesTransformer} from "ant-design-vue";
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import {theme} from 'ant-design-vue';
+
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const isDarkTheme = ref(darkModeMediaQuery.matches);
+
+darkModeMediaQuery.addEventListener('change', (e) => {
+    isDarkTheme.value = e.matches;
+    console.log(isDarkTheme.value ? '🌒 深色模式开启' : '🌖 浅色模式开启');
+});
+
+const themeAlgo = computed(() => {
+    return isDarkTheme.value ? theme.darkAlgorithm : theme.defaultAlgorithm;
+});
+
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/logo.svg" class="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
-        </a>
-    </div>
-    <HelloWorld msg="Vite + Vue"/>
+    <a-config-provider
+        :locale="zhCN"
+        :theme="{algorithm: themeAlgo}"
+    >
+        <a-style-provider
+            :transformers="[legacyLogicalPropertiesTransformer]">
+            <a-app w-full h-full>
+                <div id="app" ref="app">
+                </div>
+            </a-app>
+        </a-style-provider>
+    </a-config-provider>
 </template>
 
-<style scoped>
-
+<style>
+@import "@/assets/theme/style.css";
+@import "@/assets/theme/light.css" (prefers-color-scheme: light);
+@import "@/assets/theme/dark.css" (prefers-color-scheme: dark);
 </style>
