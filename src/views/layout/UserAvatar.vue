e645239142572bf9220fea5c4bceda7b720253a8
thelastcookies <thelastcookies@icloud.com>
2024-03-26 11:15:32 +0800

    feat: 新增用户头像组件

---
+++ src/views/layout/UserAvatar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import {message} from "ant-design-vue";
+import type {MenuInfo} from "ant-design-vue/es/menu/src/interface";
+
+withDefaults(defineProps<{
+    size: number
+}>(), {
+    size: 14
+});
+
+const userStore = useUserStore();
+const realName = computed(() => userStore.userInfo ? userStore.userInfo.realName : '');
+const appStore = useAppStore();
+
+async function handleClick({key}: MenuInfo) {
+    if (key === 'logout') {
+        const hide = message.loading('注销中，请稍候。', 0);
+        appStore.signOut().finally(() => {
+            hide();
+            message.success('注销成功', 3);
+        });
+    }
+}
+
+</script>
+
+<template>
+    <a-dropdown>
+        <div class="cursor-pointer h-full flex">
+            <a-avatar :size="size" mr-8px>
+                <BaseIcon icon="UserOutlined" :size="size"></BaseIcon>
+            </a-avatar>
+            <span class="user-name">{{ realName }}</span>
+        </div>
+        <template #overlay>
+            <a-menu @click="handleClick">
+                <a-menu-divider/>
+                <a-menu-item key="logout">
+                    <template #icon>
+                        <BaseIcon icon="LogoutOutlined"></BaseIcon>
+                    </template>
+                    注销
+                </a-menu-item>
+            </a-menu>
+        </template>
+    </a-dropdown>
+</template>
+
+<style scoped lang="less">
+.user-name {
+    font-size: ~'@{fontSizeLG}px';
+    color: @colorBgContainer;
+}
+</style>
