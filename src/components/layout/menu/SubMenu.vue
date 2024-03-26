5c300e4eb925414670b187cabe1481035ad09b13
thelastcookies <thelastcookies@icloud.com>
2024-03-26 11:18:22 +0800

    feat: 新增边栏菜单组件

---
+++ src/components/layout/menu/SubMenu.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { VNodeChild } from 'vue'
+import type {MenuTreeNode} from "@/types";
+
+defineProps<{
+    item: MenuTreeNode,
+}>();
+
+const renderTitle = (title: VNodeChild | (() => VNodeChild)) => {
+    if (typeof title === 'function') {
+        return title();
+    }
+    return title;
+}
+
+</script>
+
+<template>
+    <template v-if="item.children">
+        <a-sub-menu :key="item.key">
+            <template v-if="item.icon" #icon>
+                <BaseIcon :icon="item.icon" />
+            </template>
+            <template #title>
+                {{ renderTitle(item.title) }}
+            </template>
+            <template v-for="subM in item.children">
+                <template v-if="!subM.showInMenu">
+                    <SubMenu :key="subM!.key" :item="subM"/>
+                </template>
+            </template>
+        </a-sub-menu>
+    </template>
+    <template v-else>
+        <a-menu-item :key="item.key">
+            <template v-if="item.icon" #icon>
+                <BaseIcon :icon="item.icon" />
+            </template>
+            {{ renderTitle(item.title) }}
+        </a-menu-item>
+    </template>
+</template>
