023334042fe7d99b8aa4b8b7d44bba95ffddc27e
thelastcookies <thelastcookies@icloud.com>
2024-03-26 11:20:47 +0800

    feat: 新增 Loading 组件

---
+++ src/components/base/BaseLoading.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full h-full flex-c icon">
+        <BaseIcon icon="LoadingOutlined" size="60"></BaseIcon>
+    </div>
+</template>
+
+<style scoped lang="less">
+.icon {
+    color: @colorPrimary;
+}
+</style>
