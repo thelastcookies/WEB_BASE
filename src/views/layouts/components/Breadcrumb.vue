486d698b5fbd1c66d507421628cc8962e98e7699
thelastcookies <thelastcookies@icloud.com>
2024-03-31 02:30:38 +0800

    refactor: 使用 UnoCSS Antdv Theme 重写面包屑组件

--- src/views/layouts/components/Breadcrumb.vue
+++ src/views/layouts/components/Breadcrumb.vue
@@ -3,15 +3,9 @@ const {crtBreadcrumb} = storeToRefs(useActionStore());
 </script>
 
 <template>
-    <a-breadcrumb h-54px lh-54px p-lr-24px class="breadcrumb-container">
+    <a-breadcrumb h-54px lh-54px p-lr-24px bg-ant__bg-container>
         <template v-for="menu in crtBreadcrumb">
             <a-breadcrumb-item>{{ menu.title }}</a-breadcrumb-item>
         </template>
     </a-breadcrumb>
 </template>
-
-<style scoped lang="less">
-.breadcrumb-container {
-    background-color: var(--colorBgContainer);
-}
-</style>
