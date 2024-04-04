e9db47d33e3d88e652e692e62e903f8c2377d477
thelastcookies <thelastcookies@icloud.com>
2024-04-04 18:06:37 +0800

    fix: 修复 UserStore 没有正确地抛出异常导致 Token 过期后系统加载异常的问题

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -5,10 +5,18 @@ export const useUserStore = defineStore('user', () => {
     const userPerms = shallowRef([] as any[]);
 
     const getUserInfo = async () => {
-        // 获取用户信息
-        const {Data} = await getOperatorInfo();
-        userInfo.value = preprocessUserInfo(Data.UserInfo) as UserInfo;
-        userPerms.value = Data.Permissions;
+        return new Promise<void | Error>((resolve, reject) => {
+            // 获取用户信息
+            getOperatorInfo().then(res => {
+                if (!res.Success) {
+                    reject(new Error(WITH_UNAUTHORIZED));
+                } else {
+                    userInfo.value = preprocessUserInfo(res.Data.UserInfo) as UserInfo;
+                    userPerms.value = res.Data.Permissions;
+                    resolve();
+                }
+            }).catch(e => reject(e));
+        });
     };
 
     const $reset = () => {
