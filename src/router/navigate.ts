f54e7b990989054ffef29f14fe25f7b723545cdf
thelastcookies <thelastcookies@icloud.com>
2024-04-06 04:30:42 +0800

    feat: 将导航方法 "routeTo" Promise 化，并增加导航到菜单时的处理方法

--- src/router/navigate.ts
+++ src/router/navigate.ts
@@ -1,32 +1,36 @@
-import type {RouteToInfo} from "@/types";
+import type {ActionItem, RecordName, RouteToInfo} from "@/types";
 import type {RouteLocationRaw} from "vue-router";
 
 const actionStore = useActionStore();
 const {actionTree} = storeToRefs(actionStore);
 
 export const routeTo = (routeToInfo: RouteToInfo) => {
-    let actionId = routeToInfo.name;
-    // 使用原生 pageId 取得路由配置信息
-    const action = findAction(actionTree.value, actionId);
-    if (!action) {
-        console.error(`router.routeTo: Cannot find action by id: ${String(actionId)}.`);
-        return;
-    }
-    // 如果是外链页面，则直接跳转
-    if (action.type === MenuPageType.LINK) {
-        window.open(action.url);
-        return;
-    }
-    // 路由配置中页面没有配置实际组件，只作为菜单结构存在
-    if (!action.component && !action.url?.startsWith('/redirect')) {
-        console.warn(`router.routeTo: Cannot find 'component' in action: ${String(actionId)}.`);
-        return;
-    }
-    // 构建 Route
-    let route: RouteLocationRaw;
-    route = {name: action.menuId as string};
-    if (routeToInfo.params) {
-        Object.assign(route, {params: routeToInfo.params});
-    }
-    router.push(route);
+    return new Promise<ActionItem>((resolve, reject) => {
+        const actionId = routeToInfo.name;
+        const action = findAction(actionTree.value, actionId);
+        if (!action) {
+            const err = `Router.navigate "routeTo": Cannot find action by id: ${String(actionId)}.`
+            console.error(err);
+            reject(Error(err));
+        } else if (action.type === MenuPageType.LINK) {
+            // 如果是外链页面，则直接跳转
+            window.open(action.url);
+            resolve(action);
+        } else if (action.type === MenuPageType.MENU) {
+            // 如果是菜单页面，则找寻其直系子节点，直到某一个子节点配置了可跳转的页面
+            const descendant = findDescendantWithUrlDefined(action);
+            if (descendant) {
+                resolve(routeTo({name: descendant.menuId as RecordName}));
+            } else {
+                reject(Error(`Router.navigate "routeTo": Cannot find any descendants with a 'url' attribute defined in the action with id: ${String(actionId)}.`))
+            }
+        } else {
+            // 构建 Route
+            let route: RouteLocationRaw;
+            route = {name: action.menuId as RecordName};
+            routeToInfo.params && Object.assign(route, {params: routeToInfo.params});
+            router.push(route);
+            resolve(action);
+        }
+    });
 };
