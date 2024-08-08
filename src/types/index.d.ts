47ea4be6b3b7f0ed6bf34ea181462e36f171a986
thelastcookies <thelastcookies@icloud.com>
2024-08-08 11:31:34 +0800

    type: 新增通用的树节点的类型声明

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,17 +4,19 @@ import type { ShowInMenuType } from "@/enums";
 export type Key = string | number;
 export type RecordName = string | symbol;
 
-interface CommonItem {
-  id: string,
-  name: string,
-}
+/**
+ * 通用的树节点的类型声明，其中
+ * 作为节点唯一标识符的字段，键的可选项有 "id" 与 "Id"，值的类型为 Key；
+ * 作为父亲节点唯一标识符的字段，键的可选项有 "pid"、"pId"、"parentId" 与 "ParentId"，值的类型为 Key；
+ * 作为子级列表的字段，键的可选项有 "children 与 "Children"，值的类型为 TreeLikeItem
+ */
+export type TreeNodeIdField = "id" | "Id";
+export type TreeNodeParentIdField = "pid" | "pId" | "parentId" | "ParentId";
+export type TreeNodeChildrenField = "children" | "Children";
 
-interface ListItem extends CommonItem {
-  pId: string,
-}
-
-interface TreeNode extends ListItem {
-  children?: Array<TreeNode>
+export interface TreeLikeItem extends Partial<Record<TreeNodeIdField, Key>>,
+  Partial<Record<TreeNodeParentIdField, Key>>,
+  Partial<Record<TreeNodeChildrenField, TreeLikeItem[]>> {
 }
 
 export interface RouteToRecord {
