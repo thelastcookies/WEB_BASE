f063454ac1c4023a302a028fb9d53fb65d059db2
thelastcookies <thelastcookies@icloud.com>
2024-08-11 23:42:57 +0800

    style: 修复页面切换时出现横向滚动条的问题

--- src/views/layouts/Layout.vue
+++ src/views/layouts/Layout.vue
@@ -16,7 +16,7 @@ const { deviceType } = useAppStore();
       </a-layout-sider>
       <a-layout>
         <Breadcrumb v-if="deviceType === 'desktop'"></Breadcrumb>
-        <a-layout-content overflow-auto>
+        <a-layout-content overflow-x-hidden overflow-y-auto>
           <Parent />
         </a-layout-content>
       </a-layout>
