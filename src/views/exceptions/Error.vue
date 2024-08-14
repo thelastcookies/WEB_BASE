52963886223abdad9e76be3edc2739684ccfba53
thelastcookies <thelastcookies@icloud.com>
2024-08-14 20:25:50 +0800

    style: 增加新的异常页面图标

--- src/views/exceptions/Error.vue
+++ src/views/exceptions/Error.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
+import notFoundSvg from "~/exceptions/404.svg";
+
 const router = useRouter();
 
 function back() {
   router.replace({
-    path: '/',
+    path: "/",
   });
 }
 </script>
 
 <template>
-  <a-result status="404" title="NOT FOUND" sub-title="对不起，当前访问的页面不存在！">
+  <a-result title="NOT FOUND" sub-title="对不起，当前访问的页面不存在！">
+    <template #icon>
+      <img :src="notFoundSvg" alt="404.svg">
+    </template>
     <template #extra>
       <a-button type="primary" @click="back">
         返回首页
