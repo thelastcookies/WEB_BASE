f83f8701c67245af9eff911aa77a0402bbb4664a
thelastcookies <thelastcookies@icloud.com>
2024-08-29 15:43:01 +0800

    chore: 删除 ActionTree 示例

--- src/views/samples/Sample.vue
+++ src/views/samples/Sample.vue
@@ -5,15 +5,12 @@
  */
 import QueryFormSample from '@/views/samples/query-form/QueryFormSample.vue';
 import EChartsSample from '@/views/samples/echarts/EChartsSample.vue';
-import ActionTreeSample from '@/views/samples/action-tree/ActionTreeSample.vue';
 import InlineEditTableSample from '@/views/samples/inline-edit-table/InlineEditTableSample.vue';
 </script>
 <template>
   <QueryFormSample />
   <a-divider>ECharts</a-divider>
   <EChartsSample />
-  <a-divider>ActionTree</a-divider>
-  <ActionTreeSample />
   <a-divider>InlineEditTable</a-divider>
   <InlineEditTableSample></InlineEditTableSample>
 </template>
