b846fb6eb19e6aef4aa45c88b7f50aa5038258a7
thelastcookies <thelastcookies@icloud.com>
2024-08-29 15:34:55 +0800

    feat: 在菜单管理中引入行内编辑表格

--- src/views/settings/action/action.ts
+++ src/views/settings/action/action.ts
@@ -1,21 +1,20 @@
-import type { TableEditableColumnProps } from "@/components/common/inline-edit-table/types";
+import type { TableEditableColumnProps } from '@/components/common/inline-edit-table/types';
 
 export const permTableColumns: TableEditableColumnProps[] = [
   {
-    title: "权限名称",
-    dataIndex: "Name",
+    title: '权限名称',
+    dataIndex: 'Name',
     width: 250,
-    editable: false,
-  },
-  {
-    title: "配置",
-    dataIndex: "Value",
     editable: true,
   },
   {
-    title: "操作",
-    dataIndex: "operation",
+    title: '配置',
+    dataIndex: 'Value',
+    editable: true,
+  },
+  {
+    title: '操作',
+    dataIndex: 'operation',
     width: 160,
-    editable: true,
   },
 ];
