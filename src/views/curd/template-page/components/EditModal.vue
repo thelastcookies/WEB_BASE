69263ef4edba88c48482f8bc8412eec808a2ddd8
thelastcookies <thelastcookies@icloud.com>
2024-11-15 22:31:12 +0800

    feat: 完善 CURD 示例页面

--- src/views/curd/template-page/components/EditModal.vue
+++ src/views/curd/template-page/components/EditModal.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { EditEnum, SexEnum } from '@/enums';
+import { EditEnum } from '@/enums';
 import type { FormInstance } from 'ant-design-vue/es/form/Form';
 import { message } from 'ant-design-vue';
 import type { Rule } from 'ant-design-vue/es/form';
 import type { ValidateErrorEntity } from 'ant-design-vue/es/form/interface';
+import type { DataRecord } from '@/views/curd/template-page/types';
 
 const open = defineModel('open', { default: false });
 
 const props = withDefaults(defineProps<{
-  type: EditEnum;
-  id?: string;
+  type?: EditEnum;
+  data?: any;
 }>(), {
   type: EditEnum.VIEW,
 });
@@ -30,7 +31,7 @@ const title = computed(() => {
 });
 
 const formRef = ref<FormInstance>();
-const formData = ref<any>({});
+const formData = ref<DataRecord>({});
 
 const rules: Record<string, Rule[]> = {};
 
@@ -38,14 +39,14 @@ const handleSubmit = async () => {
   loading.value = true;
   try {
     await formRef.value?.validate();
-    const { Success, Msg } = await saveUser(formData.value);
-    if (Success) {
-      message.success('保存成功');
-      emit('submit');
-      handleClear();
-    } else {
-      message.error({ content: '保存失败，' + Msg });
-    }
+    // const { Success, Msg } = await saveUser(formData.value);
+    // if (Success) {
+    //   message.success('保存成功');
+    //   emit('submit');
+    //   handleClear();
+    // } else {
+    //   message.error({ content: '保存失败，' + Msg });
+    // }
     open.value = false;
   } catch (e) {
     if ((e as ValidateErrorEntity)?.errorFields) {
@@ -59,22 +60,12 @@ const handleSubmit = async () => {
 
 const handleClear = () => {
   formRef.value!.resetFields();
-  formData.value = {
-    Sex: SexEnum.MALE,
-    State: UserStateEnum.ENABLE,
-  };
-};
-
-const fetch = async (id: string) => {
-  if (id) {
-    const res = await getUser(id);
-    if (res.Data) formData.value = res.Data;
-  }
+  formData.value = {};
 };
 
 watch(open, (v) => {
-  if (v && props.id) {
-    fetch(props.id);
+  if (v && props.data) {
+    formData.value = merge({}, props.data);
   }
 });
 
@@ -88,30 +79,19 @@ watch(open, (v) => {
     @cancel="handleClear">
     <a-form ref="formRef" autocomplete="off" :model="formData"
       :label-col="{ span: 4 }" :wrapper-col="{ span: 18 }"
-      :rules="rules" :disabled="loading"
+      :rules="rules" :disabled="loading || type === EditEnum.VIEW"
       class="px-3 pt-3">
-      <a-form-item label="账号" name="UserName">
-        <a-input v-model:value="formData.UserName" />
+      <a-form-item label="Id" name="id">
+        <a-input v-model:value="formData.id" />
       </a-form-item>
-      <a-form-item label="姓名" name="RealName">
-        <a-input v-model:value="formData.RealName" />
+      <a-form-item label="字段1" name="field1">
+        <a-input v-model:value="formData.field1" />
       </a-form-item>
-      <a-form-item label="性别" name="Sex">
-        <a-input v-model:value="formData.Sex" />
+      <a-form-item label="字段2" name="field2">
+        <a-input v-model:value="formData.field2" />
       </a-form-item>
-      <a-form-item label="生日" name="BirthdayText">
-        <a-input v-model:value="formData.BirthdayText" />
-      </a-form-item>
-      <a-form-item label="角色" name="RoleNames">
-        <a-input v-model:value="formData.RoleNames" />
-      </a-form-item>
-      <a-form-item label="备注" name="Remark">
-        <a-input v-model:value="formData.Remark" />
-      </a-form-item>
-      <a-form-item label="状态" name="State">
-        <a-switch v-model:checked="formData.State"
-          checked-value="1" un-checked-value="0"
-          checked-children="启用" un-checked-children="禁用"></a-switch>
+      <a-form-item label="字段3" name="field3">
+        <a-input v-model:value="formData.field3" />
       </a-form-item>
     </a-form>
   </a-modal>
